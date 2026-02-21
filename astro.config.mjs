bd20b93bc1825d30cc5ffe59a9b131921818d3a7
Jim Madge <jim@jmadge.com>
2026-02-21 17:34:58 +0000

    feat(i8n): add Crowdin configuration
    Updates Crowdin configuration and adds infrastructure for building with translations. An npm script
    is added to fetch translations and placeholders are added to the astro config for serving them.
    
    fixes #902

--- astro.config.mjs
+++ astro.config.mjs
@@ -75,6 +75,151 @@ export default defineConfig({
         en: {
           label: 'English',
         },
+        // ar: {
+        //   label: 'العربية',
+        //   dir: 'rtl',
+        // },
+        // bn: {
+        //   // Bengali
+        //   label: 'বাং',
+        // },
+        // da: {
+        //   // Danish
+        //   label: 'Dansk',
+        // },
+        // de: {
+        //   // German
+        //   label: 'Deutsch',
+        // },
+        // el: {
+        //   // Greek
+        //   label: 'Νέα Ελληνικά',
+        // },
+        // es: {
+        //   // Spanish
+        //   label: 'Español',
+        // },
+        // fa: {
+        //   // Farsi (Persian)
+        //   label: 'فارسی',
+        // },
+        // fi: {
+        //   // Finnish
+        //   label: 'Suomi',
+        // },
+        // fil: {
+        //   // Filipino (Tagalog)},
+        //   label: 'Wikang Tagalog',
+        // },
+        // fr: {
+        //   // French
+        //   label: 'Français',
+        // },
+        // gl: {
+        //   // Galician
+        //   label: 'Galego',
+        // },
+        // he: {
+        //   // Hebrew
+        //   label: 'עברית',
+        // },
+        // hi: {
+        //   // Hindi
+        //   label: 'हिन्दी',
+        // },
+        // hr: {
+        //   // Croatian
+        //   label: 'Hrvatski',
+        // },
+        //id: {
+        //  //Indonesian
+        //  label: 'bahasa Indonesia',
+        //},
+        // it: {
+        //   // Italian
+        //   label: 'Italiano',
+        // },
+        // ja: {
+        //   // Japanese
+        //   label: '日本語',
+        // },
+        // ko: {
+        //   // Korean
+        //   label: '한국어, 조선말',
+        // },
+        // ml: {
+        //   // Malayalam
+        //   label: 'മലയാള',
+        // },
+        // nl: {
+        //   // Dutch, Flemish
+        //   label: 'Nederlands',
+        // },
+        // no: {
+        //   // Norwegian
+        //   label: 'Norsk',
+        // },
+        // pl: {
+        //   // Polish
+        //   label: 'Polski',
+        // },
+        // pt: {
+        //   // Portuguese
+        //   label: 'Português',
+        // },
+        // ro: {
+        //   // Romanian, Moldavian, Moldovan
+        //   label: 'Română, Ромынэ',
+        // },
+        // ru: {
+        //   // Russian
+        //   label: 'Русский язык',
+        // },
+        // si: {
+        //   // Sinhala, Sinhalese
+        //   label: 'සිංහල',
+        // },
+        // sk: {
+        //   // Slovak
+        //   label: 'Solvenčina',
+        // },
+        // sv: {
+        //   // Swedish
+        //   label: 'Svenska',
+        // },
+        // sw: {
+        //   // Swahili
+        //   label: 'Kiswahili; كِسوَحِيلِ',
+        // },
+        // ta: {
+        //   // Tamil
+        //   label: 'தமிழ்',
+        // },
+        // th: {
+        //   // Thai
+        //   label: 'ภาษาไทย',
+        // },
+        // tr: {
+        //   // Turkish
+        //   label: 'Türkçe',
+        // },
+        // uk: {
+        //   // Ukrainian
+        //   label: 'Українська (Ukraїnska)',
+        // },
+        // ur: {
+        //   // Urdu
+        //   label: 'اُردُو',
+        //   dir: 'rtl',
+        // },
+        // vi: {
+        //   // Vietnamese
+        //   label: 'tiếng Việt',
+        // },
+        // zh: {
+        //   // Chinese
+        //   label: '中文 (Zhōngwén), 汉语; 漢語 (Hànyǔ)',
+        // },
       },
     }),
     sitemap()
